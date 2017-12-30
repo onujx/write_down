29a9f2789da2643a9d8a12a8eac2d2d748e0535a
para11e1 <zxczv41@gmail.com>
2017-12-30 14:34:07 +0900

    Add files via upload

---
+++ 포인터 형성평가 1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+	
+	 
+	int a, b, i;
+	int *p, *p2;
+	p = &a;
+	p2 = &b;
+	
+	scanf("%d %d", &a, &b);
+	
+	printf("%d", *-1(abs(*p)-abs(*p2)));
+
+
+	return 0;
+	
+	
+}
